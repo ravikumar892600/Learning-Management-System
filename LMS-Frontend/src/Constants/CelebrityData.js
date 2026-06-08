2915b3e5944282366f7cddc51cfe0fc19964f825
Ravi Kumar <ravikumar892600@gmail.com>
2026-06-08 15:55:36 +0530

    Cleaned up our about us

---
+++ LMS-Frontend/src/Constants/CelebrityData.js
@@ -0,0 +1,42 @@
+import apj from "../assets/Images/apj.png";
+import billGates from "../assets/Images/billGates.png";
+import einstein from "../assets/Images/einstein.png";
+import nelsonMandela from "../assets/Images/nelsonMandela.png";
+import steveJobs from "../assets/Images/steveJobs.png";
+
+export const celebrities = [
+  {
+    title: "APJ Abdul Kalam",
+    description:
+      "Teaching is a very noble profession that shapes the character, caliber, and future of an individual.",
+    image: apj,
+    slideNumber: 1,
+  },
+  {
+    title: "Steve Jobs",
+    description:
+      "We dont get a chance to do that many things, and every one should be really excellent.",
+    image: steveJobs,
+    slideNumber: 2,
+  },
+  {
+    title: "Bill Gates",
+    description:
+      "Success is a lousy teacher. It seduces smart people into thinking they can’t lose.",
+    image: billGates,
+    slideNumber: 3,
+  },
+  {
+    title: "Nelson Mandela",
+    description:
+      "Education is the most powerful tool you can use to change the world.",
+    image: nelsonMandela,
+    slideNumber: 4,
+  },
+  {
+    title: "Albert Einstein",
+    description: "There are only two ways to live your life.",
+    image: einstein,
+    slideNumber: 5,
+  },
+];
