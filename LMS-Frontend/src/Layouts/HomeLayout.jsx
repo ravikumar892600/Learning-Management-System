e75c3a8c3999615e4ad5566efc725cd13d76c247
Ravi Kumar <ravikumar892600@gmail.com>
2026-06-08 12:09:16 +0530

    Added login signup button to sidebar

--- LMS-Frontend/src/Layouts/HomeLayout.jsx
+++ LMS-Frontend/src/Layouts/HomeLayout.jsx
@@ -1,22 +1,39 @@
+import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
 
-function HomeLayout({children}) {
+function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // For checking if user logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  // For displaying the options to role
+  const role = useSelector((state) => state?.auth?.role);
 
   function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = 'auto';
+    drawerSide[0].style.width = "auto";
   }
 
-  function hideDrawer(){
+  function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
 
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = '0';
+    drawerSide[0].style.width = "0";
+  }
 
+  function handleLogout(e) {
+    e.preventDefault();
+
+    // const res = await dispatchEvent(logout());
+
+    // if(res?payload?.success)
+    navigate("/");
   }
 
   return (
@@ -36,28 +53,57 @@ function HomeLayout({children}) {
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
             <li className="w-fit absolute right-2 z-50">
-                <button onClick={hideDrawer}>
-                    <AiFillCloseCircle size={24} />
-                </button>
+              <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={24} />
+              </button>
             </li>
             <li>
-                <Link to="/">Home</Link>
+              <Link to="/">Home</Link>
+            </li>
+            {isLoggedIn && role == "ADMIN" && (
+              <li>
+                <Link to="/admin/dashboard">Admin DashBoard</Link>
+              </li>
+            )}
+            <li>
+              <Link to="/contact">Contact Us</Link>
             </li>
             <li>
-                <Link to="/contact">Contact Us</Link>
+              <Link to="/about">About Us</Link>
             </li>
-            <li>
-                <Link to="/about">About Us</Link>
-            </li>
-           
 
+            {!isLoggedIn && (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Login</Link>
+                  </button>
+                </div>
+              </li>
+            )}
+
+            {isLoggedIn && (
+              <li className="absolute bottom-4 w-[90%]">
+                <div className="w-full flex items-center justify-center">
+                  <button className="btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className="btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link onClick={handleLogout}>Logout</Link>
+                  </button>
+                </div>
+              </li>
+            )}
           </ul>
         </div>
       </div>
 
       {children}
-      
-      <Footer/>
+
+      <Footer />
     </div>
   );
 }
